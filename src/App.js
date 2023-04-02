eabcecdc9f405c8ff37b035417bb9f44490a674f
VietNguyen <viet7896@gmail.com>
2023-04-02 21:54:01 +0700

    Define Routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import React,{useContext} from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import { ThemeContext } from './ThemeContext';
+import HomePage from './pages/HomePage';
+import PostPage from './pages/PostPage';
 
 function App() {
   const { theme } = useContext(ThemeContext);
   return (  
+    <BrowserRouter>
     <div className={`container ${theme}`}>
       <NavBar />
       <div className="main">
-        <h1> Posts </h1>
-          <ul>
-            <li><h2>Post 1 </h2><p>Post 1 content</p></li>
-            <li><h2>Post 2 </h2><p>Post 2 content</p></li>
-            </ul>
+        <Routes>
+          <Route path="/post/:postId" element = {<PostPage />} />         
+          <Route path="/" element = {<HomePage />} />      
+            </Routes>
+       
       </div>
       <div className="footer"> Nguyen Tien Viet - 20194718 </div>
     </div>
+    </BrowserRouter>
   );
 }
 
